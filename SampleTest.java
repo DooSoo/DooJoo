1753459d162a7b0f31426943376e4571938ca8a4
DooSoo <fountain8spring@gmail.com>
2017-02-20 15:53:16 -0800

    Henry 1 test

---
+++ SampleTest.java
@@ -0,0 +1,19 @@
+import org.testng.annotations.Test;
+
+
+public class SampleTest {
+	
+	@Test // testApp is our test case
+	
+	public void testApp(){
+		System.out.println("Testing app");
+		
+	}
+	
+	@Test
+	public void testLogin(){
+		System.out.println("Login Test");
+		
+	}
+
+}
